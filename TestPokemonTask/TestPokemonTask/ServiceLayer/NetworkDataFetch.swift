ba6fa50df3b2c6c709a423c3e3b043dbb36a8e34
Scooterminsk <kickscooterby@gmail.com>
2022-10-25 21:44:22 +0300

    create fetchPokemonDescription in NetworkDataFetch

--- TestPokemonTask/TestPokemonTask/ServiceLayer/NetworkDataFetch.swift
+++ TestPokemonTask/TestPokemonTask/ServiceLayer/NetworkDataFetch.swift
@@ -20,6 +20,7 @@ class NetworkDataFetch {
             case .success(let data):
                 do {
                     let pokemons = try JSONDecoder().decode(PokemonModel.self, from: data)
+                    response(pokemons, nil)
                 } catch let jsonError {
                     print("Failed to decode JSON", jsonError)
                 }
@@ -29,4 +30,23 @@ class NetworkDataFetch {
             }
         }
     }
+    
+    func fetchPokemonDescription(urlString: String, response: @escaping (PokemonDescriptionModel?, Error?) -> Void) {
+        
+        NetworkRequest.shared.requestData(urlString: urlString) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let pokemonDescription = try JSONDecoder().decode(PokemonDescriptionModel.self, from: data)
+                    response(pokemonDescription, nil)
+                } catch let jsonError {
+                    print("Failed to decode JSON", jsonError)
+                }
+            case .failure(let error):
+                print("Error received requesting data: \(error.localizedDescription)")
+                response(nil, error)
+            }
+        }
+    }
+
 }
