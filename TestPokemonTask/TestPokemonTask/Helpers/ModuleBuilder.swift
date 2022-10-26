00bc081500371ab4b95e1ff38e8075a9671b84e4
Scooterminsk <kickscooterby@gmail.com>
2022-10-26 12:45:55 +0300

    add createDetailModule to ModuleBuilder

--- TestPokemonTask/TestPokemonTask/Helpers/ModuleBuilder.swift
+++ TestPokemonTask/TestPokemonTask/Helpers/ModuleBuilder.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol Builder {
     func createPokemonListModule() -> UIViewController
+    func createDetailModule(pokemon: Pokemon?) -> UIViewController
 }
 
 class ModuleBuilder: Builder {
@@ -16,6 +17,14 @@ class ModuleBuilder: Builder {
         let view = PokemonListViewController()
         let networkService = NetworkDataFetch.shared
         let presenter = PokemonListPresenter(view: view, networkService: networkService)
+        view.presenter = presenter
+        return view
+    }
+    
+    func createDetailModule(pokemon: Pokemon?) -> UIViewController {
+        let view = DetailViewController()
+        let networkService = NetworkDataFetch.shared
+        let presenter = DetailPresenter(view: view, networkService: networkService, pokemon: pokemon)
         view.presenter = presenter
         return view
     }
