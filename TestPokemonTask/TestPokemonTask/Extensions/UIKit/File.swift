461a0be308c900d865adc05830275b26d40e703c
Scooterminsk <kickscooterby@gmail.com>
2022-11-05 15:30:56 +0300

    feat: add method for displaying message if table view is empty

---
+++ TestPokemonTask/TestPokemonTask/Extensions/UIKit/File.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  TestPokemonTask
+//
+//  Created by Zenya Kirilov on 5.11.22.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
