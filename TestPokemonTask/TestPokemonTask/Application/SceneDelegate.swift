489bd56f132ac5c294938d0b44884a2c045ee572
Scooterminsk <kickscooterby@gmail.com>
2022-11-05 15:40:29 +0300

    feat: add default message for empty pokemons table view

--- TestPokemonTask/TestPokemonTask/Application/SceneDelegate.swift
+++ TestPokemonTask/TestPokemonTask/Application/SceneDelegate.swift
@@ -15,10 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
-//        let manager = DBManager()
-//        manager.deleteAllPokemonDescriptions()
-//        manager.deleteAllPokemons()
 
         let navigationViewController = UINavigationController()
         let assemblyModuleBuilder = AssemblyModuleBuilder()
